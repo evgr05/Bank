ba6e5ddf55665ccbf55f7f4846b15700a98b4544
admin <admin@COMP4>
2024-03-20 19:35:24 +0300

    Добавьте файлы проекта.

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
